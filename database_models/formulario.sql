e1ff43fd61d6e9467dc8bf49ee7b5cc30e8f3fd4
Analice Leite <analice.leite12@gmail.com>
2025-08-30 18:26:24 -0300

    chore(docs): formulario-db.sql file updated and interest screen pic added

--- database_models/formulario.sql
+++ database_models/formulario.sql
@@ -3,19 +3,24 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 23, 2025 at 04:22 PM
+-- Generation Time: Aug 30, 2025 at 11:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
 START TRANSACTION;
+
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+/*!40101 SET NAMES utf8mb4 */
+;
 
 --
 -- Database: `formulario_db`
@@ -24,21 +29,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auth`
+-- Table structure for table `interest`
 --
 
-CREATE TABLE `auth` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+CREATE TABLE `interest` (
+  `interest_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `auth`
+-- Dumping data for table `interest`
 --
 
-INSERT INTO `auth` (`id`, `username`, `password`) VALUES
-(1, 'admin', '123');
+INSERT INTO `interest` (`interest_id`, `name`) VALUES
+(2, 'Esportes'),
+(3, 'Programação');
 
 -- --------------------------------------------------------
 
@@ -47,14 +52,13 @@ INSERT INTO `auth` (`id`, `username`, `password`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `birth_date` date NOT NULL,
   `address` text NOT NULL,
   `state` varchar(100) NOT NULL,
   `gender` varchar(20) NOT NULL,
-  `interests` text DEFAULT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -65,45 +69,84 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `birth_date`, `address`, `state`, `gender`, `interests`, `username`, `password`, `created_at`, `photo`) VALUES
-(21, 'Joao Testandou', 'joao@teeaaa', '2025-08-06', 'adasadasd', 'Rio de Janeiro', 'Feminino', '[\"Moda\",\"Tecnologia\",\"Esportes\",\"M\\u00fasica\"]', 'teste', '$2y$10$tuj5h18MTt6o6UVWDHclwO9TZ1G/UP7yyqt3m2TgJiWN/ZC6mXVKa', '2025-08-21 01:00:41', 'uploads/photos/photo_68a6709f3804b3.52997334.jpg');
+INSERT INTO `users` (`user_id`, `name`, `email`, `birth_date`, `address`, `state`, `gender`, `username`, `password`, `created_at`, `photo`) VALUES
+(22, 'Analice Leite', 'analice.leite12@gmail.com', '2025-07-28', 'Rua Jabuti 357 Itinga Araquari', 'Santa Catarina', 'Feminino', 'nalice', '$2y$10$OCXFmTU5nHUcrPdDIz8D5OSgJmKzvDZMdTDFJ/wGP3pifAeBA8XrG', '2025-08-29 14:26:21', 'uploads/photos/photo_68b1b88da1df27.39814352.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_interest`
+--
+
+CREATE TABLE `user_interest` (
+  `user_id` int(11) NOT NULL,
+  `interest_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_interest`
+--
+
+INSERT INTO `user_interest` (`user_id`, `interest_id`) VALUES
+(22, 2),
+(22, 3);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `auth`
+-- Indexes for table `interest`
 --
-ALTER TABLE `auth`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+ALTER TABLE `interest` ADD PRIMARY KEY (`interest_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `user_interest`
+--
+ALTER TABLE `user_interest`
+  ADD PRIMARY KEY (`user_id`,`interest_id`),
+  ADD KEY `fk_interest` (`interest_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `auth`
+-- AUTO_INCREMENT for table `interest`
 --
-ALTER TABLE `auth`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `interest`
+  MODIFY `interest_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_interest`
+--
+ALTER TABLE `user_interest`
+  ADD CONSTRAINT `fk_interest` FOREIGN KEY (`interest_id`) REFERENCES `interest` (`interest_id`),
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
